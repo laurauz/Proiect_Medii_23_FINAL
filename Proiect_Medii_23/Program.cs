62d816e74f6ef79ae7700132f4dc50b39f7b5db6
laura <laura@DESKTOP-R1HGH24>
2023-01-12 11:43:30 +0200

    Lab5Done

--- Proiect_Medii_23/Program.cs
+++ Proiect_Medii_23/Program.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Proiect_Medii_23.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Proiect_Medii_23Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Proiect_Medii_23Context") ?? throw new InvalidOperationException("Connection string 'Proiect_Medii_23Context' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Proiect_Medii_23Context") ?? 
+    throw new InvalidOperationException("Connectionstring 'Proiect_Medii_23Context' not found.")));
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options =>options.SignIn.RequireConfirmedAccount = true)
+//.AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
@@ -22,6 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
